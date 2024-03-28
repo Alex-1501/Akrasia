cf7c51dfe0c2f692636f0d9a3d6ecd7fa00d1bf5
Alex0247 <59071533+Alex0247@users.noreply.github.com>
2024-03-28 01:00:12 +0000

    Added Removal Of Clients

--- akrasia.py
+++ akrasia.py
@@ -4,9 +4,14 @@ import threading
 
 client_list = []
 
-def startListener(HOST, PORT):
-    connection_list = []
+def clear():
+    print("\033[H\033[J")
 
+def printMenu():
+    print("[1] List Active Clients")
+    print("[2] Remove Connected Client\n")
+
+def startListener(HOST, PORT):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     s.listen(5)
@@ -21,11 +26,45 @@ def startListener(HOST, PORT):
              client_list.append(conn)
         
 
-def activeClients():
-     for client in client_list:
-        print(f"Client: {client.getpeername()}")    
+def activeClients(flag):
+     counter = 0
+
+     if len(client_list) < 1:
+          print("Error: No Active Clients Connected\n")
+
+     if len(client_list) >= 1 and flag == "1":
+        clear()
+        printMenu()
+        for client in client_list:
+            print(f"\nClient: {client.getpeername()}")
+     elif len(client_list) >= 1 and flag == "2":
+          clear()
+          printMenu()
+          for client in client_list:
+            counter = counter + 1
+            print(f"[{counter}] {client.getpeername()}\n")
+
+def removeClient():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    selectedClientStr = input("\nTo Remove A Client, Type In Their Number: ")
+    selectedClient = int(selectedClientStr)
+    x = client_list[selectedClient - 1]
+    if x in client_list:
+        try:
+            x.close()
+            client_list.remove(x)
+            clear()
+            print("\nClient Removed Successfully\n")
+        except:
+            clear()
+            print("\nError: Could Not Remove Client\n")
+
+
 
 def main(HOST, PORT):
+    isRunning = True
+    menu = True
     print("""                             
    _____     __                                .__           
   /  _  \   |  | __ _______  _____      ______ |__| _____    
@@ -37,10 +76,25 @@ def main(HOST, PORT):
     threading.Thread(target=startListener, args=(HOST, PORT)).start()
     print(f"Listening: {HOST} : {PORT}\n")
 
-    print("[1] List Active Clients")
-    userInput = input("Enter Here: ")
-    if userInput == "1":
-            client_thread = threading.Thread(target=activeClients).start()
+    while(isRunning):
+        # Print menu but once a user selects an option, it will not be printed again
+        if menu:
+            printMenu()
+            menu = False
+
+        option = input("Select An Option: ")
+
+        if option == "1":
+            activeClients(option)
+        elif option == "2":
+            activeClients(option)
+            removeClient()
+            printMenu()
+        else:
+            print("Invalid Option\n")
+            menu = True
+
+
 
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
